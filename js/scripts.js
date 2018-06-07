7bdc60cebc38c10b71e5ebaaacf40fa141c8d994
Nate Cottle and Rita Bennett-Chew <rita@>
2018-06-07 09:46:08 -0700

    Account for 1-3

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var one = "I";
+var five = "V";
+var ten = "X";
+var fifty = "L";
+var hundred = "C";
+var fiveHundred = "D";
+var thousand = "M";
+
+function toRomNum(input) {
+  return one + one;
+}
+
+toRomNum(2)
