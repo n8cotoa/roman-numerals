5534350822e26d3de0439ee54bf3d533ad3ed548
Nate Cottle and Rita Bennett-Chew <rita@>
2018-06-07 12:01:47 -0700

    account for any 10 digit for the 1-3 ones place

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,30 @@ var c = "C";
 var d = "D";
 var m = "M";
 
-// function find1To3(input) {
-//   var outputArray = []
-//
-//   // works for inputs 1 - 3
-//   for (j = 1; j <= input; ++j) {
-//     outputArray.push(i)
-//   }
-//   return outputArray.join("")
-// }
+function makeReverseUserArray(inputNum) {
+  var userArray = inputNum.toString().split("").reverse();
+  return userArray
+}
+
+makeUserArray(49)
+
+function findOnesPlace(userArray) {
+  return makeReverseUserArray(userArray)[0]
+}
+
+function translateOnesPlace(){
+  
+}
+
+function find1To3(input) {
+  var outputArray = []
+
+  // works for inputs 1 - 3
+  for (j = 1; j <= input; ++j) {
+    outputArray.push(i)
+  }
+  return outputArray.join("")
+}
 //
 // function find20Or30(input) {
 //   var outputArray = []
@@ -24,8 +39,8 @@ var m = "M";
 //   return outputArray.join("")
 // }
 
-// Translates 1 - 3, and 10, 20, 30 by branching 
-function find1To3Or10Or30(input){
+// Translates 1 - 3, and 10, 20, 30 by branching
+function find1To3Or10Or30or50To80(input){
   var outputArray = []
   if (input <= 3) {
     for (j = 1; j <= input; ++j) {
@@ -37,7 +52,7 @@ function find1To3Or10Or30(input){
     outputArray.push(x);
     }
     return outputArray.join("")
-  }
+  } else if (input === )
 }
 
 function find4Or9(input) {
@@ -65,6 +80,24 @@ function findTeens(input) {
   }
 }
 
+function findTwoDigit(input) {
+  // works for inputs 11 - 19
+  var tens = parseInt(makeUserArray(input)[0])
+  console.log(tens, typeof(tens))
+  if ((input <= parseInt(tens.toString() + 3)) && (input >= parseInt((tens.toString() + 1)))) {
+    var xString = ""
+    for (b = 1; b <= 3; ++b) {
+      xString = xString.concat(x);
+    }
+    console.log("xString", xString)
+    return xString + find1To3Or10Or30(input - (tens * 10))
+  } else if (input === 14 || input === 19) {
+    return x + find4Or9(input - 10)
+  } else if ((input <= 18) && (input >= 16)) {
+    return x + find6To8(input - 10)
+  }
+}
+
 function biquinary(input) {
     if (input === 10) {
     return x
@@ -79,4 +112,4 @@ function biquinary(input) {
   }
 }
 
-find20Or30(10)
+findTwoDigit(33)
